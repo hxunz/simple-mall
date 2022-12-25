b142cfa0c61ff6a8e75c48de47d2404c28adce8c
hxunz <guswl2843@gmail.com>
2022-12-26 01:37:05 +0900

    장바구니 목록과 아이템 컴포넌트 분리 후 상품 수량에 따른  금액 기능 구현

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,24 +1,17 @@
 import styled from '@emotion/styled';
-import { Box, CardActionArea, CardContent, Typography, Checkbox, ButtonGroup, Button } from '@mui/material';
-import { useAppSelector } from 'hooks';
-import Image from 'next/image';
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { useAppDispatch, useAppSelector } from 'hooks';
 import { productItems } from 'productItem';
 import { useState } from 'react';
+import { loadCoupons } from 'redux/productsSlice';
+import CartItem from './cartItem';
 
 const Cart = () => {
-  const [quantity, setQuantity] = useState(1);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  const { cartProducts } = useAppSelector(store => store.products);
+  const { cartProducts, coupons } = useAppSelector(store => store.products);
 
   const cartList = productItems.filter(product => cartProducts.includes(product.item_no));
-
-  const handleClickIncreaseQuantity = () => {
-    setQuantity(quantity + 1)
-  };
-
-  const handleClickDecreaseQuantity = () => {
-    setQuantity(quantity - 1)
-  };
 
   return (
     <>
@@ -30,35 +23,17 @@ const Cart = () => {
           detail_image_url,
           price
         }) => (
-          <CardWrapper key={item_no}>
-            <Checkbox />
-            <Box>
-              <CardActionArea>
-                <Image
-                  width={400}
-                  height={400}
-                  src={detail_image_url}
-                  alt={item_name}
-                />
-                <CardContent>
-                  <Typography>
-                    {item_name}
-                  </Typography>
-                  <CardText>
-                    {price}
-                  </CardText>
-                </CardContent>
-              </CardActionArea>
-            </Box>
-            <ButtonGroup variant='outlined'>
-              <Button onClick={quantity === 1 ? undefined : handleClickDecreaseQuantity}>-</Button>
-              <Button>{quantity}</Button>
-              <Button onClick={handleClickIncreaseQuantity}>+</Button>
-            </ButtonGroup>
-          </CardWrapper>
+          <CartItem
+            key={item_no}
+            item_name={item_name}
+            detail_image_url={detail_image_url}
+            price={price}
+            setTotalPrice={setTotalPrice}
+          />
         ))}
       </Wrapper>
       <p>총 결제 금액</p>
+      <p>{totalPrice}</p>
     </>
   )
 }
@@ -69,20 +44,6 @@ const Wrapper = styled(Box)`
   min-height: 30rem;
 `;
 
-const CardWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  padding: 1rem;
-  margin-top: 2rem;
-`;
 
-const CardText = styled(Typography)`
-  display: -webkit-box;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical;
-  font-size: 14;
-  overflow: hidden;
-`;
 
 export default Cart;
