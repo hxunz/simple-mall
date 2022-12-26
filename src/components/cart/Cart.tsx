82c6f96f032024474bb60d8e7f31aaa209a0bc88
hxunz <guswl2843@gmail.com>
2022-12-26 12:35:42 +0900

    장바구니 페이지 UI 수정

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,5 +1,4 @@
-import styled from '@emotion/styled';
-import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { FormControl, InputLabel, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { useAppSelector } from 'hooks';
 import { productItems } from 'productItem';
 import { useState } from 'react';
@@ -12,36 +11,59 @@ const Cart = () => {
 
   const cartList = productItems.filter(product => cartProducts.includes(product.item_no));
 
+  const handleChangeCoupon = () => {
+    // if (title === '10% 할인 쿠폰')
+    // console.log('hi');
+  }
+
+  const titles = coupons.map((it) => it.title);
+
   return (
     <>
-      <p>cart</p>
-      <Wrapper>
-        {cartList.map(({
-          item_no,
-          item_name,
-          detail_image_url,
-          price
-        }) => (
-          <CartItem
-            key={item_no}
-            item_name={item_name}
-            detail_image_url={detail_image_url}
-            price={price}
-            setTotalPrice={setTotalPrice}
-          />
-        ))}
-      </Wrapper>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label='simple-table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>체크</TableCell>
+              <TableCell>상품 정보</TableCell>
+              <TableCell>수량</TableCell>
+              <TableCell>주문 금액</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {cartList.map(({
+              item_no,
+              item_name,
+              detail_image_url,
+              price
+            }) => (
+              <CartItem
+                key={item_no}
+                item_name={item_name}
+                detail_image_url={detail_image_url}
+                price={price}
+                setTotalPrice={setTotalPrice}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <p>쿠폰 적용</p>
       <FormControl fullWidth>
-        <InputLabel>쿠폰</InputLabel>
+        <InputLabel id='select-label'>쿠폰</InputLabel>
         <Select
-          value={coupons}
-          label="coupon"
+          labelId='select-label'
+          id='select'
+          label='Coupons'
+          value={titles}
+          onChange={handleChangeCoupon}
         >
           {coupons.map(({
             title
           }) => (
-            <MenuItem key={title}>{title}</MenuItem>
+            <MenuItem key={title} value={title}>
+              {title}
+            </MenuItem>
           ))}
         </Select>
       </FormControl>
@@ -50,11 +72,5 @@ const Cart = () => {
     </>
   )
 }
-
-const Wrapper = styled(Box)`
-  display: flex;
-  flex-wrap: wrap;
-  min-height: 30rem;
-`;
 
 export default Cart;
--- src/components/cart/cartItem/CartItem.tsx
+++ src/components/cart/cartItem/CartItem.tsx
@@ -1,5 +1,4 @@
-import styled from '@emotion/styled';
-import { Box, CardActionArea, CardContent, Typography, Checkbox, ButtonGroup, Button } from '@mui/material';
+import { Checkbox, ButtonGroup, Button, TableRow, TableCell } from '@mui/material';
 import Image from 'next/image';
 import { Dispatch, SetStateAction, useState } from 'react';
 
@@ -32,49 +31,45 @@ const CartItem: React.FC<Props> = ({ item_name, detail_image_url, price, setTota
   }
 
   return (
-    <CardWrapper>
-      <Checkbox checked={checked} onClick={handleClickAddPay} />
-      <Box>
-        <CardActionArea>
+    <TableRow>
+      <TableCell align='center'>
+        <div>
+          <Checkbox checked={checked} onClick={handleClickAddPay} />
+        </div>
+      </TableCell>
+
+      <TableCell align='center'>
+        <div>
           <Image
             width={400}
             height={400}
             src={detail_image_url}
             alt={item_name}
           />
-          <CardContent>
-            <Typography>
-              {item_name}
-            </Typography>
-            <CardText>
-              {price}
-            </CardText>
-          </CardContent>
-        </CardActionArea>
-      </Box>
-      <ButtonGroup variant='outlined'>
-        <Button onClick={quantity === 1 ? undefined : handleClickDecreaseQuantity}>-</Button>
-        <Button>{quantity}</Button>
-        <Button onClick={handleClickIncreaseQuantity}>+</Button>
-      </ButtonGroup>
-    </CardWrapper>
+        </div>
+        <div>
+          {item_name}
+        </div>
+      </TableCell>
+
+      <TableCell align='center'>
+        <div>
+          {price}
+        </div>
+      </TableCell>
+
+      <TableCell align='center'>
+        <div>
+          <ButtonGroup variant='outlined'>
+            <Button onClick={quantity === 1 ? undefined : handleClickDecreaseQuantity}>-</Button>
+            <Button>{quantity}</Button>
+            <Button onClick={handleClickIncreaseQuantity}>+</Button>
+          </ButtonGroup>
+        </div>
+      </TableCell>
+
+    </TableRow>
   )
 };
-
-const CardWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  padding: 1rem;
-  margin-top: 2rem;
-`;
-
-const CardText = styled(Typography)`
-  display: -webkit-box;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical;
-  font-size: 14;
-  overflow: hidden;
-`;
 
 export default CartItem;
