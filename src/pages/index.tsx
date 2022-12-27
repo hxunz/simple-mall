251efe7ad9be56c6e7a15707934eafd0b91af14d
hxunz <guswl2843@gmail.com>
2022-12-28 01:04:55 +0900

    사용하지 않는 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,6 @@
-import { Button } from '@mui/material';
-import { useRouter } from 'next/router';
-
 const Home = () => {
-  const router = useRouter();
-
-  const handleClickRouteProductsPage = () => {
-    router.push('/products')
-  }
-
   return (
-    <Button onClick={handleClickRouteProductsPage}>
-      BEST
-    </Button>
+    <></>
   )
 }
 
