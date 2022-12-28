31a60e0f6199b40e884719a2977fe4254fccff76
hxunz <guswl2843@gmail.com>
2022-12-28 11:20:03 +0900

    UI 수정

--- src/components/dialog/AlertDialog.tsx
+++ src/components/dialog/AlertDialog.tsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/router';
 import { FC } from 'react';
 import { IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import styled from '@emotion/styled';
 
 export type AlertDialogProps = {
   open: boolean;
@@ -31,19 +32,44 @@ const AlertDialog: FC<AlertDialogProps> = ({ open, onClose, message }) => {
       open={open}
       onClose={handleClose}
     >
-      <IconButton onClick={handleClose} sx={{ width: '40px' }}>
+      <CustomIconButton
+        onClick={handleClose}
+      >
         <CloseIcon />
-      </IconButton>
+      </CustomIconButton>
       <DialogContent>
-        <DialogContentText id="alert-dialog-description">
+        <CustomDialogContentText>
           {message}
-        </DialogContentText>
+        </CustomDialogContentText>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleMoveCart}>장바구니 바로가기</Button>
+        <CustomButton onClick={handleMoveCart}>
+          장바구니 바로가기
+        </CustomButton>
       </DialogActions>
     </Dialog>
   );
 }
+
+const CustomIconButton = styled(IconButton)`
+  margin: .5rem 0 0 15rem;
+  width: 60px;
+  color: black;
+`
+const CustomDialogContentText = styled(DialogContentText)`
+  font-size: 18px;
+  color: black;
+  line-height: 1.4em;
+  text-align: center;
+`
+
+const CustomButton = styled(Button)`
+  border: 1px solid rgb(212, 212, 212);
+  border-radius: 2px;
+  font-size: 14px;
+  color: rgb(93, 93, 93);
+  line-height: 30px;
+  margin: 1px auto 15px;
+`
 
 export default AlertDialog;
